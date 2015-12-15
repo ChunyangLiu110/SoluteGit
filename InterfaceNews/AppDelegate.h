149fb0b145fcb14f0a0668de9b4d1167a10d5200
qianfeng <qianfeng@simida.local>
2015-12-15 09:26:28 +0800

    Initial Commit

---
+++ InterfaceNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InterfaceNews
+//
+//  Created by qianfeng on 15/12/15.
+//  Copyright © 2015年 Jackson Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InterfaceNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InterfaceNews
+//
+//  Created by qianfeng on 15/12/15.
+//  Copyright © 2015年 Jackson Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
